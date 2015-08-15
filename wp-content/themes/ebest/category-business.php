6f47531367edc87744b734e25f58cb158ac544c8
Uice Lu <uicestone@gmail.com>
2015-08-16 00:58:17 +0800

    fix #12

--- wp-content/themes/ebest/category-business.php
+++ wp-content/themes/ebest/category-business.php
@@ -1,4 +1,11 @@
-<?php global $page_class; $page_class = 'page-business'; get_header(); ?>
+<?php 
+global $page_class; $page_class = 'page-business';
+get_header();
+add_action('parse_query', function($wp_query){
+	$wp_query->set('orderby', 'ID');
+	$wp_query->set('order', 'ASC');
+});
+?>
 
 <section class="banner" style="background-image:url('<?=get_stylesheet_directory_uri() . '/img/' . $wp_query->query_vars['category_name']?>-banner.jpg')">
 	<div class="text">
@@ -34,7 +41,7 @@
 						<div class="row">
 							<div class="col-md-12 col-xs-6">
 								<?php foreach(get_posts(array('cat'=>$wp_query->query_vars['cat'], 'order'=>'ASC', 'posts_per_page'=>-1)) as $index => $post){ ?>
-								<a href="#<?=$post->post_name?>" class="list-group-item<?php if($index === 0){ ?> active<?php } ?>"><?=$post->post_title?></a>
+								<a href="#<?=$post->post_name?>" class="list-group-item"><?=$post->post_title?></a>
 								<?php } ?>
 							</div>
 						</div>
@@ -42,7 +49,7 @@
 				</div>
 			</div>
 			<?php while(have_posts()): the_post(); ?>
-			<div id="<?=get_post(get_the_ID())->post_name?>" class="content col-sm-12 col-md-9">
+			<div id="<?=get_post(get_the_ID())->post_name?>" class="content col-sm-12 col-md-9" style="display:none">
 				<?php the_content(); ?>
 			</div>
 			<?php endwhile; ?>
@@ -53,10 +60,12 @@
 <script type="text/javascript">
 jQuery(function($){
 	if(window.location.hash){
-		$('.content').hide();
 		$('.content' + window.location.hash).show();
-		$('.list-group-item').removeClass('active');
 		$('.list-group-item[href="' + window.location.hash + '"]').addClass('active');
+	}
+	else{
+		$('.content:first').show();
+		$('.list-group-item:first').addClass('active');
 	}
 	$('.list-group-item').on('click', function(event){
 		event.preventDefault();
