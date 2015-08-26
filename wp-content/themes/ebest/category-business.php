836a6a551946c00a52b2f8c6c5acf531fa7907b3
Uice Lu <uicestone@gmail.com>
2015-08-26 21:16:56 +0800

    fix #20

--- wp-content/themes/ebest/category-business.php
+++ wp-content/themes/ebest/category-business.php
@@ -50,7 +50,7 @@ add_action('parse_query', function($wp_query){
 			</div>
 			<?php while(have_posts()): the_post(); ?>
 			<div id="<?=get_post(get_the_ID())->post_name?>" class="content col-sm-12 col-md-9" style="display:none">
-				<?=parse_tabs(wpautop(get_the_content()))?>
+				<?=parse_tabs(wpautop(get_the_content()), get_the_title())?>
 			</div>
 			<?php endwhile; ?>
 		</div>
