7461c354a573c0b16c6b12bbbe31a860e6c38c8d
Uice Lu <uicestone@gmail.com>
2016-01-28 22:30:47 -0900

    WordPress 4.4.1; 插件和主题更新

--- wp-includes/js/mediaelement/wp-mediaelement.js
+++ wp-includes/js/mediaelement/wp-mediaelement.js
@@ -1,85 +1,61 @@
 /* global mejs, _wpmejsSettings */
-(function ($) {
+(function( window, $ ) {
+
+	window.wp = window.wp || {};
+
 	// add mime-type aliases to MediaElement plugin support
 	mejs.plugins.silverlight[0].types.push('video/x-ms-wmv');
 	mejs.plugins.silverlight[0].types.push('audio/x-ms-wma');
 
-	$(function () {
+	function wpMediaElement() {
 		var settings = {};
 
-		if ( typeof _wpmejsSettings !== 'undefined' ) {
-			settings = _wpmejsSettings;
+		/**
+		 * Initialize media elements.
+		 *
+		 * Ensures media elements that have already been initialized won't be
+		 * processed again.
+		 *
+		 * @since 4.4.0
+		 */
+		function initialize() {
+			if ( typeof _wpmejsSettings !== 'undefined' ) {
+				settings = _wpmejsSettings;
+			}
+
+			settings.success = settings.success || function (mejs) {
+				var autoplay, loop;
+
+				if ( 'flash' === mejs.pluginType ) {
+					autoplay = mejs.attributes.autoplay && 'false' !== mejs.attributes.autoplay;
+					loop = mejs.attributes.loop && 'false' !== mejs.attributes.loop;
+
+					autoplay && mejs.addEventListener( 'canplay', function () {
+						mejs.play();
+					}, false );
+
+					loop && mejs.addEventListener( 'ended', function () {
+						mejs.play();
+					}, false );
+				}
+			};
+
+			// Only initialize new media elements.
+			$( '.wp-audio-shortcode, .wp-video-shortcode' )
+				.not( '.mejs-container' )
+				.filter(function () {
+					return ! $( this ).parent().hasClass( '.mejs-mediaelement' );
+				})
+				.mediaelementplayer( settings );
 		}
 
-		settings.success = settings.success || function (mejs) {
-			var autoplay, loop;
-
-			if ( 'flash' === mejs.pluginType ) {
-				autoplay = mejs.attributes.autoplay && 'false' !== mejs.attributes.autoplay;
-				loop = mejs.attributes.loop && 'false' !== mejs.attributes.loop;
-
-				autoplay && mejs.addEventListener( 'canplay', function () {
-					mejs.play();
-				}, false );
-
-				loop && mejs.addEventListener( 'ended', function () {
-					mejs.play();
-				}, false );
-			}
+		return {
+			initialize: initialize
 		};
+	}
 
-		// settings.pluginWidth = 480;
-		// settings.pluginHeight = 240;
-		// settings.defaultVideoWidth = 480;
-		// settings.defaultVideoHeight = 240;
-		settings.enableAutosize = false;
+	window.wp.mediaelement = new wpMediaElement();
 
-		var tmp = '<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">' +
-'		  <div class="modal-dialog">' +
-'		    <div class="modal-content">' +
-'		      <div class="modal-body">' +
-'		      </div>' +
-'		    </div>' +
-'		  </div>' +
-'		</div>';
+	$( document ).on( 'ready', window.wp.mediaelement.initialize );
 
-		function initModal(i) {
-			return function() {
-				var $video = $(this).parent().find("video").clone(),
-						$modal = $(tmp).attr("id", "video-" + i ),
-						player;
-				$modal.find('.modal-body').html($video)
-					.end().appendTo('body').modal()
-
-				// fire only once
-				$modal.one('shown.bs.modal', function() {
-					// $modal.find(".wp-video-shortcode").mediaelementplayer( settings ).show();
-					$modal.find(".wp-video-shortcode").attr("id", "modal-video-" + i).show()
-					player = new MediaElementPlayer( "#modal-video-" + i, settings);
-					// player.play();
-				})
-
-				$modal.on('hidden.bs.modal', function(e) {
-					player.pause();
-				})
-			}
-		}
-
-		function handleVideo(i) {
-			var src = $(this).attr("poster"),
-					$img = $("<img class='bg' src=" + src + " />"),
-					$overlay = $('<div class="mejs-overlay mejs-layer mejs-overlay-play"><div class="mejs-overlay-button"></div></div>');
-			$(this).hide().before($img).before($overlay)
-
-			$overlay.one("click", initModal(i))
-
-			$overlay.on("click", function(e) {
-				$("#video-" + i).modal('show')
-			})
-			
-		}
-
-		$('.wp-audio-shortcode, .wp-video-shortcode').each(handleVideo)
-	});
-
-}(jQuery));
+})( window, jQuery );
